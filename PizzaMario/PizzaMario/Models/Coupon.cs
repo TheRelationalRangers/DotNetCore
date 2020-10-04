c8c23bc260571cbabff03976053fad9176bc5e0c
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-10-04 12:52:33 +0200

    Add Connectionstring

--- PizzaMario/PizzaMario/Models/Coupon.cs
+++ PizzaMario/PizzaMario/Models/Coupon.cs
@@ -9,5 +9,13 @@ namespace PizzaMario.Models
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public decimal FlatDiscount { get; set; }
+        public decimal PercentageDiscount { get; set; }
+    }
+
+    public class ProductCoupon : Entity
+    {
+        public Product Product { get; set; }
     }
 }
